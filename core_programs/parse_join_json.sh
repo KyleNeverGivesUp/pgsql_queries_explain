37adb20dbc84f6560237fd943a88416f536cbc9b
KyleAtASU <shunjieh@asu.edu>
2025-02-10 03:54:40 -0700

    developing

--- core_programs/parse_join_json.sh
+++ core_programs/parse_join_json.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+SQL_DIR="/home/pgsql_explain/sql_files/output/pgsql_queries_explain/cleansed_json_files"
+OUTPUT_DIR="/home/pgsql_explain/sql_files/output/parsed_join_json_files"
+
+mkdir -p "$OUTPUT_DIR"
+
+for json_file in "$SQL_DIR"/*format_json_cleansed
+do
+  echo "$json_file"
+
+done
+
