d7c06cb5511bd2b5a271050d80ec253afb729e1a
KyleAtASU <shunjieh@asu.edu>
2025-02-10 11:25:15 -0700

    testing

--- core_programs/parse_join_json.sh
+++ core_programs/parse_join_json.sh
@@ -22,7 +22,7 @@ for json_file in "$SQL_DIR"/*format_json_cleansed; do
     echo "Processing: $json_file -> $output_file"
 
     # Execute Python script and save output to the correct file
-    python "$SCRIPT_DIR"/parse_explain_json_dev.py "$json_file" > "$output_file"
+    python "$SCRIPT_DIR/parse_explain_json_dev.py" "$json_file" > "$output_file"
 
 done
 
