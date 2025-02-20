8cfe062508407adf9e1c6c5b21dd6679ea6fcc83
KyleAtASU <shunjieh@asu.edu>
2025-02-19 21:31:20 -0700

    Update Comments

--- core_programs/parse_explain_json_dev.py
+++ core_programs/parse_explain_json_dev.py
@@ -367,7 +367,6 @@ if __name__ == "__main__":
     # print(script_dir)
     # file_name = "21c_explain_verbose_analyze_format_json_cleansed"
     # file_path = script_dir / file_name
-    # print(file_path)
     res = []
     backup = []
     table = {}
