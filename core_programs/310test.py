16d840c77944b9e3e454c0bc288396b5ade1e9d7
KyleAtASU <shunjieh@asu.edu>
2025-03-04 19:17:51 -0700

    Update code

--- core_programs/310test.py
+++ core_programs/310test.py
@@ -2,8 +2,8 @@ def Mystery(n):
     sum=0
     for i in range(1,n+1):
         for j in range(1,i+1):
-            # print(f"i'm i: {i}")
-            print(f"i'm j: {j}")
+            print(f"i'm i: {i}")
+            # print(f"i'm j: {j}")
             print(f"==============")
             print(f"==============")
             print(f"==============")
