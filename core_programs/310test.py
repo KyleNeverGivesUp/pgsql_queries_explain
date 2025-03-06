e1ef43340aa91ad7551a54c17c222da3a83b21f5
KyleAtASU <shunjieh@asu.edu>
2025-03-06 10:05:56 -0700

    Update code

--- core_programs/310test.py
+++ core_programs/310test.py
@@ -6,7 +6,7 @@ def Mystery(n):
             print(f"i'm j: {j}")
             print(f"==============")
             print(f"==============")
-            # print(f"==============")
+            print(f"==============")
             # sum=sum+1
     return sum
 if "__main__" == __name__:
