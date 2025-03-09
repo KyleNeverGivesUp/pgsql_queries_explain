b3b8891cf4dc78beb9510d5483105751bbfa0265
KyleAtASU <shunjieh@asu.edu>
2025-03-09 03:42:14 -0700

    Update code

--- core_programs/310test.py
+++ core_programs/310test.py
@@ -2,12 +2,12 @@ def Mystery(n):
     sum=0
     for i in range(1,n+1):
         for j in range(1,i+1):
-            # print(f"i'm i: {i}")
+            print(f"i'm i: {i}")
             print(f"i'm j: {j}")
             print(f"==============")
             print(f"==============")
             print(f"==============")
-            # sum=sum+1
+            sum=sum+1
     return sum
 if "__main__" == __name__:
     print(Mystery(3))
